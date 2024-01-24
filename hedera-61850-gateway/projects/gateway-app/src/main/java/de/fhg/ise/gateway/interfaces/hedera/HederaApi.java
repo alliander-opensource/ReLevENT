a962ea8a937a0a1441a4492c3a71c3a0920903bf
Daniel Werner <daniel.werner@ise.fraunhofer.de>
2024-01-24 09:37:06 +0100

    Make application more robust - reconnect to DER in case of connection loss during runtime (initial check will still abort Execution) - delete all existing schedules from HEDERA before creating new schedules (instead of cleaning up after creating new schedules) - increase timeout for HEDERA calculations to 5 minutes (was 100s before)

--- hedera-61850-gateway/projects/gateway-app/src/main/java/de/fhg/ise/gateway/interfaces/hedera/HederaApi.java
+++ hedera-61850-gateway/projects/gateway-app/src/main/java/de/fhg/ise/gateway/interfaces/hedera/HederaApi.java
@@ -1,6 +1,8 @@
 package de.fhg.ise.gateway.interfaces.hedera;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import de.fhg.ise.gateway.Context;
 import de.fhg.ise.gateway.HederaException;
 import de.fhg.ise.gateway.configuration.Settings;
@@ -14,7 +16,7 @@ import io.swagger.client.model.Period;
 import io.swagger.client.model.Point;
 import io.swagger.client.model.Quantity;
 import io.swagger.client.model.RegisteredInterTie;
-import io.swagger.client.model.Schedule;
+import io.swagger.client.model.Schedule.AtTypeEnum;
 import io.swagger.client.model.ScheduleGetResponse;
 import io.swagger.client.model.SchedulePostResponse;
 import io.swagger.client.model.ScheduleRequest;
@@ -29,15 +31,22 @@ import org.threeten.bp.ZoneId;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -50,6 +59,7 @@ public class HederaApi {
 
     private static final Logger log = LoggerFactory.getLogger(HederaApi.class);
     private final ScheduleApi api;
+    private String lastLoginBearer;
 
     public HederaApi(String clientId, String clientSecret) throws IOException {
         try {
@@ -67,10 +77,11 @@ public class HederaApi {
         ApiClient defaultClient = Configuration.getDefaultApiClient();
         // Configure OAuth2 access token for authorization: oauth2
         OAuth oauth2 = (OAuth) defaultClient.getAuthentication("oauth2");
-        oauth2.setAccessToken(getOAuthAccessToken(clientId, clientSecret));
-        String auth = "Bearer " + oauth2.getAccessToken().replaceAll("\"", "");
-
-        log.debug("BEARER token recieved which is needed to access HEDERA Api: {}", auth);
+        String accessToken = getOAuthAccessToken(clientId, clientSecret);
+        lastLoginBearer =accessToken.replaceAll("\"","");
+        log.debug("BEARER token: {}", lastLoginBearer);
+        oauth2.setAccessToken(lastLoginBearer);
+        String auth = "Bearer " + oauth2.getAccessToken();
 
         ApiClient apiClient = new ApiClient();
         apiClient.addDefaultHeader("Content-Type", "application/vnd.hedera.v1+json");
@@ -117,7 +128,8 @@ public class HederaApi {
     public HederaSchedule requestExtensionAwaitCalculation(Instant start, HederaScheduleInterval interval,
             List<Double> values, HederaDirection direction, Settings settings) throws HederaException {
 
-        final Duration durationUntilAbort = Duration.ofSeconds(10 * 10);
+        // TODO: as setting!
+        final Duration durationUntilAbort = Duration.ofMinutes(5);
         Optional<UUID> scheduleId = Optional.empty();
 
         try {
@@ -131,7 +143,7 @@ public class HederaApi {
                 throw new HederaException("No scheduleId returned by HEDERA after schedule creation. Parameters:" + //
                         " start=" + start + ",interval=" + interval + ",values=" + values + ",direction=" + direction);
             }
-            log.info("Requesting schedule in kW: {}", values);
+            log.info("Requesting schedule in W: {}", values);
         } catch (HederaException e) {
             throw e;
         } catch (Exception e) {
@@ -142,24 +154,37 @@ public class HederaApi {
             HederaSchedule schedule = awaitScheduleCalculationAtHedera(scheduleId, durationUntilAbort);
             log.info("Result schedule in kW:   {}", schedule.getValues());
             return schedule;
-        } catch (HederaException e) {
-            throw e;
         } catch (Exception e) {
-            throw new HederaException("Error calculating schedule at HEDERA", e);
+            try {
+                log.info("Deleting corrupt schedule with mrid={}", scheduleId);
+                this.deleteSchedule(scheduleId.get());
+            } catch (Exception ex) {
+                log.warn("Unable to delete corrupt schedule with mrid={}. Reason {}:{}", scheduleId, ex.getClass(),
+                        ex.getMessage());
+            }
+            throw new HederaException(
+                    "HEDERA was unable to calculate schedule. Stopped with " + e.getClass() + ": " + e.getMessage());
         }
     }
 
     private HederaSchedule awaitScheduleCalculationAtHedera(Optional<UUID> scheduleId, Duration durationUntilAbort)
             throws HederaException {
-        Schedule.AtTypeEnum status;
+        AtTypeEnum status;
         HederaSchedule schedule;
         int count = 0;
-        final int maxCounts = 10;
-        final long pollrate = durationUntilAbort.toMillis() / maxCounts;
+        Instant start = Instant.now();
+        final Instant end = start.plus(durationUntilAbort);
+        final long pollrateMillis = 5_000;
+        boolean inital = true;
         try {
+            log.info("Will now start polling HEDERA with an interval of {}s until calculation is completed.",
+                    pollrateMillis / 1000.0);
             do {
-                // give hedera a bit time to calculate the schedules
-                Thread.sleep(pollrate);
+                if (!inital) {
+                    // give hedera a bit time to calculate the schedules
+                    Thread.sleep(pollrateMillis);
+                }
+                inital = false;
 
                 //Reading the created schedule at HEDERA
                 schedule = readSchedule(scheduleId.get());
@@ -169,14 +194,21 @@ public class HederaApi {
                         schedule.getStatusMessage());
 
                 count++;
-                log.info("Read HEDERA API " + count + " of " + maxCounts
-                        + ". Schedule calculation is currently pending. Will try again in " + pollrate / 1000.0 + "s.");
-                if (Schedule.AtTypeEnum.DECLINED.equals(status)) {
+                log.info("Read HEDERA API {} times. Schedule calculation is currently in state '{}'", count, status);
+                if (AtTypeEnum.DECLINED.equals(status)) {
                     throw new HederaException("Schedule was rejected by HEDERA. Reason as provided by HEDERA: '"
                             + schedule.getStatusMessage() + "'.");
                 }
-            } while ((count < maxCounts) && (!Schedule.AtTypeEnum.ACCEPTED.equals(status)));
-            return schedule;
+            } while ((Instant.now().isBefore(end)) && (!AtTypeEnum.ACCEPTED.equals(status)));
+
+            if (AtTypeEnum.ACCEPTED.equals(status)) {
+                log.info("Calculation at HEDERA finished. Took {}s",
+                        Duration.between(start, Instant.now()).toSeconds());
+                return schedule;
+            }
+            else {
+                throw new HederaException("Schedule calculation at hedera took too long");
+            }
         } catch (HederaException e) {
             throw e;
         } catch (Exception e) {
@@ -240,7 +272,7 @@ public class HederaApi {
         ScheduleRequest body = new ScheduleRequest().addRegisteredInterTiesItem(
                 getRegisteredInterTies(mrid, start, interval, values, direction)); // ScheduleRequest |
         log.trace("JSON body sent to create Schedule:\n" + body.toString());
-        log.info("Trying to create a schedule at HEDERA...");
+        log.debug("Trying to create a schedule at HEDERA...");
         SchedulePostResponse result = api.schedulePost(ACCEPT_HEADER, body);
         log.info("Successfully created a schedule at HEDERA.");
         return result.getScheduleReference().getMRID();
@@ -256,10 +288,67 @@ public class HederaApi {
     }
 
     public void deleteSchedule(UUID result_mRID) throws ApiException {
-        log.info("Trying to delete the schedule at HEDERA...");
+        log.debug("Trying to delete the schedule at HEDERA...");
 
         api.scheduleMRIDDelete(result_mRID, ACCEPT_HEADER);
-        log.info("Successfully deleted a schedule at HEDERA");
+        log.debug("Successfully deleted a schedule at HEDERA");
+    }
+
+    /**
+     * This is a very simplistic implementation of an HEDERA API v2 call.
+     *
+     * TODO: rather use autogenerated v2 code here (but the API changed, so this will need a bit of more work)
+     */
+    public Collection<MinimalSchedule> getScheduleMRIDsOfAllExistingSchedules() throws IOException {
+        HttpURLConnection connection = null;
+        try {
+            int pageSize = Integer.MAX_VALUE;
+            String urlString = "https://api.hedera.alliander.com/schedule?pageSize=" + pageSize;
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Authorization", "Bearer " + lastLoginBearer);
+            connection.setRequestProperty("accept", "application/vnd.hedera.v1+json");
+            connection.setRequestProperty("Content-Type", "application/vnd.hedera.v1+json");
+            int responseCode = connection.getResponseCode();
+            InputStream responseStream = connection.getInputStream();
+            Scanner s = new Scanner(responseStream).useDelimiter("\\A");
+            String response = s.hasNext() ? s.next() : "";
+            JsonObject convertedObject = Context.GSON.fromJson(response, JsonObject.class);
+            JsonElement schedules = convertedObject.get("schedules");
+            Type listOfMyClassObject = new TypeToken<ArrayList<MinimalSchedule>>() {}.getType();
+
+            List<MinimalSchedule>existingSchedules = Context.GSON.fromJson(schedules, listOfMyClassObject);
+            log.debug("Got response code {} and {} existing schedules",responseCode,existingSchedules.size());
+            return existingSchedules;
+        }
+        finally {
+            if(connection!=null){
+                connection.disconnect();
+            }
+        }
+    }
+
+    public static class MinimalSchedule {
+        UUID mRID;
+        AtTypeEnum status;
+
+        @Override
+        public String toString() {
+            return "Schedule mRID="+mRID+" ("+status+")";
+        }
+        public void setmRID(String mRID) {
+            this.mRID = UUID.fromString(mRID);
+        }
+
+        public void setStatus(String status){
+            AtTypeEnum atTypeEnum = AtTypeEnum.fromValue(status);
+            if(atTypeEnum == null){
+                this.status = AtTypeEnum.UNKNOWN;
+            }else
+                this.status=atTypeEnum;
+        }
     }
 
     /**
@@ -275,5 +364,4 @@ public class HederaApi {
         log.trace("Converted {} into {}", javaTime, convertedOffsetDateTime);
         return convertedOffsetDateTime;
     }
-
 }
--- hedera-61850-gateway/projects/gateway-app/src/main/java/de/fhg/ise/gateway/interfaces/hedera/HederaRefresh.java
+++ hedera-61850-gateway/projects/gateway-app/src/main/java/de/fhg/ise/gateway/interfaces/hedera/HederaRefresh.java
@@ -1,18 +1,24 @@
 package de.fhg.ise.gateway.interfaces.hedera;
 
-import com.beanit.iec61850bean.ServiceError;
 import de.fhg.ise.IEC61850.client.models.AllianderDER;
 import de.fhg.ise.gateway.HederaException;
 import de.fhg.ise.gateway.configuration.Settings;
 import de.fhg.ise.gateway.interfaces.GridConnectionExtension;
 import de.fhg.ise.gateway.interfaces.ems.DTO.ExtensionRequest;
 import io.swagger.client.ApiException;
+import io.swagger.client.model.Schedule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.ConnectException;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 /**
@@ -23,39 +29,56 @@ public class HederaRefresh {
     private static final Logger log = LoggerFactory.getLogger(HederaRefresh.class);
     private final Settings settings;
     private AllianderDER der;
-
     private final HederaApi api;
+    // TODO: from settings!
+    private final int scheduleNumber = 1;
+    private final int prio = 20;
+    private final int divisor = 60;
 
     private Optional<GridConnectionExtension> lastRequest = Optional.empty();
-    private Runnable hederaScheduleDeleteJob = () -> {
-        log.info("No schedule listed to be cleaned up");
-    };
 
     public HederaRefresh(HederaApi api, AllianderDER der, Settings settings) {
         this.api = api;
         this.settings = settings;
         this.der = der;
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            shutdown();
-            log.info("Done shutting down");
-        }));
-        log.trace("Shutdown hook added");
     }
 
-    public void newRequestFromEms(ExtensionRequest req) {
+    // TODO: make non-blocking!
+    public void newRequestFromEms(ExtensionRequest req)  {
         log.info("Got new request {}", req);
 
         log.debug("Cleaning up old schedules at HEDERA");
-        hederaScheduleDeleteJob.run();
-        log.info("Cleaned up old schedules at HEDERA");
+        AtomicInteger cnt = new AtomicInteger(0);
+
+        Collection<Schedule.AtTypeEnum> scheduleStatusesThatMayInterfereWithNewSchedules = Arrays.asList(Schedule.AtTypeEnum.ACCEPTED, Schedule.AtTypeEnum.PENDING,
+                Schedule.AtTypeEnum.DECLINED);
+        try {
+            api.getScheduleMRIDsOfAllExistingSchedules().forEach(schedule -> {
+                if(scheduleStatusesThatMayInterfereWithNewSchedules.contains(schedule.status)) {
+
+                    try {
+                        api.deleteSchedule(schedule.mRID);
+                        cnt.incrementAndGet();
+                    } catch (Exception e) {
+                        log.warn("Unable to delete schedule with mrid={} (state={}). This might lead to issues.",
+                                schedule.mRID, schedule.status);
+                    }
+                }
+                else  {
+                    log.trace("Ignoring schedule {}: status will not interfere with creation of new schedules",schedule);
+                }
+            });
+        }
+        catch (Exception e){
+            throw  new RuntimeException(e);
+        }
+        log.info("Cleaned up {} old schedules at HEDERA", cnt.get());
 
         HederaSchedule hederaSchedule = null;
         try {
             hederaSchedule = req.requestExtensionAwaitCalculation(this.api, this.settings);
         } catch (HederaException e) {
             log.error("Error in schedule {}. Error message by HEDERA: {}", req, e.getMessage());
-            log.debug("Error details", e);
         } catch (Exception e) {
             log.warn("Unable to create schedule at HEDERA. Retrying. Reason: {}:{}", e.getClass(), e.getMessage());
             try {
@@ -70,20 +93,6 @@ public class HederaRefresh {
         }
         else {
 
-            final HederaSchedule finalHederaSchedule = hederaSchedule;
-            hederaScheduleDeleteJob = () -> {
-                try {
-                    api.deleteSchedule(finalHederaSchedule);
-                } catch (ApiException e) {
-                    throw new RuntimeException("Unable to delete schedule at HEDERA", e);
-                }
-            };
-
-            // TODO: from settings!
-            int scheduleNumber = 1;
-            int prio = 200;
-            int divisor = 60;
-
             List<Number> values = hederaSchedule.getValues()
                     .stream()
                     .collect(Collectors.toList()); // List<Double> -> List<Number> seems to need that
@@ -91,26 +100,31 @@ public class HederaRefresh {
                 this.der.writeAndEnableSchedule(der.maxPowerSchedules.prepareSchedule(values, scheduleNumber,
                         hederaSchedule.getInterval().getAsDuration().dividedBy(divisor), hederaSchedule.getStart(),
                         prio));
-            } catch (ServiceError | IOException e) {
-                // TODO: maybe retry?
-                log.error("Unable to forward schedule to DER", e);
+                log.info("Transmitted schedule to DER. Schedule will start to run in @ {}", hederaSchedule.getStart());
+            } catch (Exception e) {
+                log.warn(
+                        "Unable to forward schedule to DER @ {}:{}. Reason: {}:{}. Trying to solve the problem by a reconnect.",
+                        der.host, der.port, e.getClass(), e.getMessage());
+
+                try {
+                    this.der = this.der.reconnect();
+                    log.info("Reconnected successfully.");
+                    this.der.writeAndEnableSchedule(der.maxPowerSchedules.prepareSchedule(values, scheduleNumber,
+                            hederaSchedule.getInterval().getAsDuration().dividedBy(divisor), hederaSchedule.getStart(),
+                            prio));
+                    log.info("Transmitted schedule to DER. Schedule will start to run in @ {}",
+                            hederaSchedule.getStart());
+                } catch (UnknownHostException | ConnectException ex) {
+                    log.error("Unable to reconnect to host '{}': {}:{}. Giving up.", der.host, ex.getClass(),
+                            ex.getMessage());
+                } catch (Exception ex) {
+                    log.error("Unable to reconnect and forward schedule to DER @ {}:{}. Giving up.", der.host, der.port,
+                            ex);
+                }
             }
         }
 
         // TODO: start a refresh here
         // TODO make use of recommended refresh time (shall be in response for the requests)
-
-        // TODO: this should be covered by the delete job but is not for some reason
-        hederaScheduleDeleteJob.run();
-    }
-
-    public void shutdown() {
-        log.debug("Starting cleanup job");
-        try {
-            this.hederaScheduleDeleteJob.run();
-            log.info("Shut down successfully");
-        } catch (Exception e) {
-            log.warn("Was unable to delete schedules at hedera", e);
-        }
     }
 }
